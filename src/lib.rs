0d802691beb399ffc5efc88d2f94e826517df282
Pedro Nascimento <pedrolcn@gmail.com>
2020-04-21 21:01:08 -0300

    feat: implement cpf validation

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,80 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+static CPF_BLACKLIST: [&'static str; 10] = [
+    "00000000000",
+    "11111111111",
+    "22222222222",
+    "33333333333",
+    "44444444444",
+    "55555555555",
+    "66666666666",
+    "77777777777",
+    "88888888888",
+    "99999999999",
+];
+
+struct CPF {}
+
+impl CPF {
+    pub fn is_valid(cpf: &[u16]) -> bool {
+        let digits = &cpf[..9];
+        let checksum = CPF::compute_checksum(digits);
+
+        cpf[9] == checksum[0] &&
+        cpf[10] == checksum[1]
+    }
+
+    fn compute_checksum(cpf_digits: &[u16]) -> [u16; 2] {
+        let first_digit = cpf_digits.iter()
+            .enumerate()
+            .map(|(idx, digit)| *digit * (10 - idx as u16))
+            .sum::<u16>() * 10 % 11 % 10;
+
+        let second_digit = (2 * first_digit + cpf_digits.iter()
+            .enumerate()
+            .map(|(idx, digit)| *digit * (11 - idx as u16))
+            .sum::<u16>()) * 10 % 11 % 10;
+
+        [first_digit, second_digit]
+    }
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, cpf-wasm!");
+pub fn isValid(cpf: String) -> Result<bool, JsValue> {
+    if CPF_BLACKLIST.iter().any(|bad_cpf| bad_cpf == &cpf.as_str()) {
+        return Ok(false)
+    }
+
+    let digits = cpf.chars().map(|c| c.to_digit(10).unwrap() as u16).collect::<Vec<_>>();
+    Ok(CPF::is_valid(&digits[..]))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn known_valid_cpf() {
+        assert_eq!(
+            isValid("04324015120".to_owned()).unwrap(),
+            true
+        )
+    }
+
+    #[test]
+    fn off_by_one() {
+        assert_eq!(
+            isValid("04324015121".to_owned()).unwrap(),
+            false
+        )
+    }
+
+    #[test]
+    fn bad_cpf() {
+        assert_eq!(
+            isValid("11111111111".to_owned()).unwrap(),
+            false
+        )
+    }
 }
